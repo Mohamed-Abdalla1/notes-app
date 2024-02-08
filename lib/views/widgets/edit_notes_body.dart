8689868c6c32b94a67296726f87a494866f615c1
Mohamed-Abdalla1 <mohamedfathalla2772@gmail.com>
2024-02-08 16:37:10 +0200

    create note model

--- lib/views/widgets/edit_notes_body.dart
+++ lib/views/widgets/edit_notes_body.dart
@@ -2,27 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/custom_appbar.dart';
 import 'package:notes_app/views/widgets/custom_textfield.dart';
 
-
 class EditNotesBody extends StatelessWidget {
   const EditNotesBody({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const Padding(
-      padding: EdgeInsets.symmetric(horizontal : 16.0),
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
       child: Column(
         children: [
           SizedBox(
             height: 50,
           ),
-          CustomAppBar(title: 'Edit Notes', icon: Icons.update),
+          CustomAppBar(
+              title: 'Edit Notes', icon: Icons.drive_file_rename_outline),
           SizedBox(
             height: 80,
           ),
           CustomTextField(title: 'Title'),
-          SizedBox(height: 16,),
-          CustomTextField(title: 'Content' , maxLines: 5,),
-
+          SizedBox(
+            height: 16,
+          ),
+          CustomTextField(
+            title: 'Content',
+            maxLines: 5,
+          ),
         ],
       ),
     );
