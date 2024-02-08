1e9ddcf1b939927f814d76066f5246524dd22439
Mohamed-Abdalla1 <mohamedfathalla2772@gmail.com>
2024-02-08 06:46:11 +0200

    add modal bottom sheet

--- lib/views/notes-view.dart
+++ lib/views/notes-view.dart
@@ -8,10 +8,30 @@ class NotesView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+            context: context,
+            builder: (context) {
+              return AddModalButtomSheet();
+            },
+          );
+        },
         child: Icon(Icons.add),
       ),
       body: NotesViewBody(),
     );
+  }
+}
+
+class AddModalButtomSheet extends StatelessWidget {
+  const AddModalButtomSheet({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
