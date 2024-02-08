97ec7b87381697afca0e3c0f636a417a8c094635
Mohamed-Abdalla1 <mohamedfathalla2772@gmail.com>
2024-02-08 09:20:59 +0200

    create edit notes body

---
+++ lib/views/widgets/edit_notes_body.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/custom_appbar.dart';
+import 'package:notes_app/views/widgets/custom_textfield.dart';
+
+
+class EditNotesBody extends StatelessWidget {
+  const EditNotesBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal : 16.0),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 50,
+          ),
+          CustomAppBar(title: 'Edit Notes', icon: Icons.update),
+          SizedBox(
+            height: 80,
+          ),
+          CustomTextField(title: 'Title'),
+          SizedBox(height: 16,),
+          CustomTextField(title: 'Content' , maxLines: 5,),
+
+        ],
+      ),
+    );
+  }
+}
