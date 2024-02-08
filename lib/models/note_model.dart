daa5c236f6a94e600e6371709cacaa8720a51721
Mohamed-Abdalla1 <mohamedfathalla2772@gmail.com>
2024-02-08 16:49:57 +0200

    add hive

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,12 +1,17 @@
+
+
+
+
 class NoteModel {
   final String title;
   final String subTitle;
   final String date;
   final int color;
 
-  NoteModel(
-      {required this.title,
-      required this.subTitle,
-      required this.date,
-      required this.color});
+  NoteModel({
+    required this.title,
+    required this.subTitle,
+    required this.date,
+    required this.color,
+  });
 }
