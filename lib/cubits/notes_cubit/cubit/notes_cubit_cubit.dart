a13c7ae3b8404b99e74eef484d446dd5dbc3a10d
Mohamed-Abdalla1 <mohamedfathalla2772@gmail.com>
2024-02-11 07:27:02 +0200

    add fetchAllNotes method to notes cubit

--- lib/cubits/notes_cubit/cubit/notes_cubit_cubit.dart
+++ lib/cubits/notes_cubit/cubit/notes_cubit_cubit.dart
@@ -1,9 +1,27 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
+import 'package:notes_app/constants.dart';
 import 'package:notes_app/models/note_model.dart';
 
 part 'notes_cubit_state.dart';
 
 class NotesCubitCubit extends Cubit<NotesCubitState> {
   NotesCubitCubit() : super(NotesCubitInitial());
+
+  fetchAllNotes(){
+   
+
+    try {
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+      List<NoteModel>noteList = notesBox.values.toList();
+      emit(NotesSuccess(noteList: noteList));
+    } catch (e) {
+      emit(
+        NotesFailure(
+          errMessage: e.toString(),
+        ),
+      );
+    }
+  }
 }
